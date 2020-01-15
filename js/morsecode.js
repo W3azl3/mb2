e2372512895226e3b8fd8d05d5106031cbac74d0
Maximilian Gündra <maximilianguendra@t-online.de>
2020-01-15 15:31:50 +0100

    update Morsecode

--- js/morsecode.js
+++ js/morsecode.js
@@ -1,8 +1,10 @@
 function alphabet(buchstabe){
     switch(buchstabe){
         case 'A':
+            console.log('Erkannter Code für A');
             return ['s','l']
         case 'B':
+            console.log('Erkannter Code für B');
             return ['l','s','s','s']
     }
 }
