ba023dbc3db7d9566b0aba2725cd763fb60411bd
Maximilian Gündra <maximilianguendra@t-online.de>
2020-01-18 11:12:14 +0100

    Tested. Letter To Code. Display only

--- cordova_plugins.js
+++ cordova_plugins.js
@@ -51,6 +51,7 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     "cordova-plugin-whitelist": "1.3.4",
     "cordova-plugin-browsersync-gen2": "1.1.7",
     "cordova-plugin-camera": "4.1.0",
-    "cordova-plugin-geolocation": "4.0.2"
+    "cordova-plugin-geolocation": "4.0.2",
+    "cordova-plugin-vibration": "3.1.1"
   };
 });
