06771e6e2cf270d3cb191ef57881d3c8d973e668
Maximilian Gündra <maximilianguendra@t-online.de>
2020-01-25 12:06:59 +0100

    Replaced framework7 alert

--- js/spiel.js
+++ js/spiel.js
@@ -84,9 +84,13 @@ function spielErgebnis(solution){
     console.log('Zwischenergebnis: ' + solution);
     console.log('randomPosition: ' + randomPosition);
     if (solution==randomPosition) {
-        alert("Richtig erkannt!");
+        alertButtonText = 'Grandiose Leistung von Dir!';
+        alertButtonTitle = 'Korrekt!';
+        // alert("Richtig erkannt!");
     } else {
-        alert("Leider nicht richtig erkannt!")
+        alertButtonText = 'Noch ein Versuch?';
+        alertButtonTitle = 'Falsch!';
+        // alert("Leider nicht richtig erkannt!")
     }
     // console.log('Ergnis korrekt!');
     // console.log('Ergnis falsch!');
